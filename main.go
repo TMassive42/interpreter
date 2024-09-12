487e9b551a2662aeff99efaeae45bbad639aee67
togondol <obimbira60@gmail.com>
2024-09-12 12:20:27 +0300

    feat: add new input string

--- main.go
+++ main.go
@@ -8,8 +8,19 @@ import (
 
 func main() {
     input := `
-function add(x int, y int) int {
-    output x + y
+function exampleFunction(arr array, obj object) string {
+    num int = 5
+    pi float = 3.14
+    text string = "Hello, Kisumu!"
+    isTrue bool = true
+    
+    if not isNull(obj.get("key")) {
+        newArr array = arr.append(num)
+        result float = pow(pi, 2)
+        output concat(text, round(result).toString())
+    }
+    
+    output null
 }
 `
     lex := lexer.NewLexer(input)
