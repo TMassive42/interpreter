aef64c34ebc6e0b111e868b748ee083eab37aa84
togondol <obimbira60@gmail.com>
2024-09-24 14:15:50 +0300

    test: add additional test cases

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,126 +1,126 @@
 package lexer
 
 import (
-	"testing"
-
-	"kisumu/token"
+    "kisumu/token"
+    "testing"
 )
 
 func TestNextToken(t *testing.T) {
-	input := `let five = 5;
-let ten = 10;
-
-let add = fn(x, y) {
-  x + y;
-};
-
-let result = add(five, ten);
-!-/*5;
-5 < 10 > 5;
-
-if (5 < 10) {
-	return true;
-} else {
-	return false;
+    input := `
+function exampleFunction(arr array, obj object) string {
+    num int = 5
+    pi float = 3.14
+    text string = "Hello, Kisumu!"
+    isTrue bool = true
+    isFalse bool = false
+    if not isNull(obj.get("key")) {
+        newArr array = arr.append(num)
+        result float = pow(pi, 2)
+        output concat(text, round(result).toString())
+    }
+    output null
 }
-
-10 == 10;
-10 != 9;
 `
 
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
-		{token.LET, "let"},
-		{token.IDENT, "five"},
-		{token.ASSIGN, "="},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "ten"},
-		{token.ASSIGN, "="},
-		{token.INT, "10"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "add"},
-		{token.ASSIGN, "="},
-		{token.FUNCTION, "fn"},
-		{token.LPAREN, "("},
-		{token.IDENT, "x"},
-		{token.COMMA, ","},
-		{token.IDENT, "y"},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.IDENT, "x"},
-		{token.PLUS, "+"},
-		{token.IDENT, "y"},
-		{token.SEMICOLON, ";"},
-		{token.RBRACE, "}"},
-		{token.SEMICOLON, ";"},
-		{token.LET, "let"},
-		{token.IDENT, "result"},
-		{token.ASSIGN, "="},
-		{token.IDENT, "add"},
-		{token.LPAREN, "("},
-		{token.IDENT, "five"},
-		{token.COMMA, ","},
-		{token.IDENT, "ten"},
-		{token.RPAREN, ")"},
-		{token.SEMICOLON, ";"},
-		{token.BANG, "!"},
-		{token.MINUS, "-"},
-		{token.SLASH, "/"},
-		{token.ASTERISK, "*"},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.INT, "5"},
-		{token.LT, "<"},
-		{token.INT, "10"},
-		{token.GT, ">"},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.IF, "if"},
-		{token.LPAREN, "("},
-		{token.INT, "5"},
-		{token.LT, "<"},
-		{token.INT, "10"},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RETURN, "return"},
-		{token.TRUE, "true"},
-		{token.SEMICOLON, ";"},
-		{token.RBRACE, "}"},
-		{token.ELSE, "else"},
-		{token.LBRACE, "{"},
-		{token.RETURN, "return"},
-		{token.FALSE, "false"},
-		{token.SEMICOLON, ";"},
-		{token.RBRACE, "}"},
-		{token.INT, "10"},
-		{token.EQ, "=="},
-		{token.INT, "10"},
-		{token.SEMICOLON, ";"},
-		{token.INT, "10"},
-		{token.NOT_EQ, "!="},
-		{token.INT, "9"},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
-	}
+    tests := []struct {
+        expectedType    token.TokenType
+        expectedLiteral string
+    }{
+        {token.FUNCTION, "function"},
+        {token.IDENT, "exampleFunction"},
+        {token.LPAREN, "("},
+        {token.IDENT, "arr"},
+        {token.ARRAY_TYPE, "array"},
+        {token.COMMA, ","},
+        {token.IDENT, "obj"},
+        {token.OBJECT_TYPE, "object"},
+        {token.RPAREN, ")"},
+        {token.STRING_TYPE, "string"},
+        {token.LBRACE, "{"},
+        {token.IDENT, "num"},
+        {token.INT_TYPE, "int"},
+        {token.ASSIGN, "="},
+        {token.INT, "5"},
+        {token.IDENT, "pi"},
+        {token.FLOAT_TYPE, "float"},
+        {token.ASSIGN, "="},
+        {token.FLOAT, "3.14"},
+        {token.IDENT, "text"},
+        {token.STRING_TYPE, "string"},
+        {token.ASSIGN, "="},
+        {token.STRING, "Hello, Kisumu!"},
+        {token.IDENT, "isTrue"},
+        {token.BOOL_TYPE, "bool"},
+        {token.ASSIGN, "="},
+        {token.BOOL, "true"},
+        {token.IDENT, "isFalse"},
+        {token.BOOL_TYPE, "bool"},
+        {token.ASSIGN, "="},
+        {token.BOOL, "false"},
+        {token.IF, "if"},
+        {token.NOT, "not"},
+        {token.IDENT, "isNull"},
+        {token.LPAREN, "("},
+        {token.IDENT, "obj"},
+        {token.DOT, "."},
+        {token.IDENT, "get"},
+        {token.LPAREN, "("},
+        {token.STRING, "key"},
+        {token.RPAREN, ")"},
+        {token.RPAREN, ")"},
+        {token.LBRACE, "{"},
+        {token.IDENT, "newArr"},
+        {token.ARRAY_TYPE, "array"},
+        {token.ASSIGN, "="},
+        {token.IDENT, "arr"},
+        {token.DOT, "."},
+        {token.IDENT, "append"},
+        {token.LPAREN, "("},
+        {token.IDENT, "num"},
+        {token.RPAREN, ")"},
+        {token.IDENT, "result"},
+        {token.FLOAT_TYPE, "float"},
+        {token.ASSIGN, "="},
+        {token.IDENT, "pow"},
+        {token.LPAREN, "("},
+        {token.IDENT, "pi"},
+        {token.COMMA, ","},
+        {token.INT, "2"},
+        {token.RPAREN, ")"},
+        {token.OUTPUT, "output"},
+        {token.IDENT, "concat"},
+        {token.LPAREN, "("},
+        {token.IDENT, "text"},
+        {token.COMMA, ","},
+        {token.IDENT, "round"},
+        {token.LPAREN, "("},
+        {token.IDENT, "result"},
+        {token.RPAREN, ")"},
+        {token.DOT, "."},
+        {token.IDENT, "toString"},
+        {token.LPAREN, "("},
+        {token.RPAREN, ")"},
+        {token.RPAREN, ")"},
+        {token.RBRACE, "}"},
+        {token.OUTPUT, "output"},
+        {token.NULL_TYPE, "null"},
+        {token.RBRACE, "}"},
+        {token.EOF, ""},
+    }
 
-	l := New(input)
+    l := NewLexer(input)
 
-	for i, tt := range tests {
-		tok := l.NextToken()
+    for i, tt := range tests {
+        tok := l.NextToken()
 
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Type)
-		}
+        if tok.Type != tt.expectedType {
+            t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+                i, tt.expectedType, tok.Type)
+        }
 
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
-				i, tt.expectedLiteral, tok.Literal)
-		}
-	}
+        if tok.Literal != tt.expectedLiteral {
+            t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+                i, tt.expectedLiteral, tok.Literal)
+        }
+    }
 }
